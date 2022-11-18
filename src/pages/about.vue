fb5d8bcd7603d324bb069a4c22c6cb58ad0f31a4
Linnzh <reg.lynnzh@gmail.com>
2022-11-18 15:02:01 +0800

    comment: 补充 home & about 页面/组件

---
+++ src/pages/about.vue
@@ -0,0 +1,13 @@
+<template>
+  <h2>About Us</h2>
+</template>
+
+<script>
+export default {
+  name: "AppAbout"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/pages/home.vue
@@ -0,0 +1,13 @@
+<template>
+  <h2>Home</h2>
+</template>
+
+<script>
+export default {
+  name: "AppHome"
+}
+</script>
+
+<style scoped>
+
+</style>
