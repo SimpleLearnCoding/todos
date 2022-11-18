dddfde67d176e6382c3876b42d9712b00589e006
Linnzh <reg.lynnzh@gmail.com>
2022-11-18 16:53:05 +0800

    comment: 使用 v-bind 在 style 中直接使用 JavaScript 中的变量

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -45,6 +45,7 @@ import {computed, ref} from "vue";
  * 返回的 count 变量就是响应式的数据
  */
 let count = ref(1)
+let color = ref('#42b983')
 
 /**
  * 使用 add 函数实现数字的修改
@@ -52,6 +53,7 @@ let count = ref(1)
  */
 function add() {
   count.value++
+  color.value = Math.random() > 0.5 ? '#42b983' : '#55c8d7';
 }
 
 /**
@@ -138,6 +140,7 @@ function useTodos() {
 
 <style scoped>
 h3 {
-  color: #42b983;
+  /* 使用 v-bind 在 style 中直接使用 JavaScript 中的变量 */
+  color: v-bind(color);
 }
 </style>
