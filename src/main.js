e6286273703b6acb742103b56feab4929a0c5607
Linnzh <reg.lynnzh@gmail.com>
2022-11-18 14:55:45 +0800

    comment: 加载 router 配置

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import router from "./router/index";
+
+/**
+ * 使用 use() 加载 router 配置
+ */
+createApp(App).use(router).mount('#app')
---
+++ src/router/index.js
@@ -0,0 +1,41 @@
+import {
+    createRouter,
+    createWebHashHistory,
+} from 'vue-router'
+import Home from '../pages/home.vue'
+import About from '../pages/about.vue'
+
+/**
+ * 定义页面路由
+ *
+ * @type {[{path: string, component, name: string}, {path: string, component, name: string}]}
+ */
+const routes = [
+    {
+        path: '/',
+        name: 'Home',
+        component: Home
+    },
+    {
+        path: '/about',
+        name: 'About',
+        component: About
+    }
+]
+
+/**
+ * 新建路由实例
+ * @type {Router}
+ */
+const router = createRouter({
+
+    /**
+     * 配置内部使用 hash 模式的路由
+     * -- url 会使用 # 符号来区分
+     */
+    history: createWebHashHistory(),
+
+    routes
+})
+
+export default router
