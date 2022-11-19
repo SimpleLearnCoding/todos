23ad6cbfa2f37f09c372e8127412b00dec74d0fb
Linnzh <reg.lynnzh@gmail.com>
2022-11-19 17:45:43 +0800

    comment: 使用 CSS 属性 - animation 配合 @keyframes 来设置动画效果

---
+++ src/components/MoveAnimation.vue
@@ -0,0 +1,40 @@
+<template>
+    <!--  需求：一个来回滑动的方块  -->
+    <div class="box"></div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+.box {
+    width: 30px;
+    height: 30px;
+    background: #42b983;
+
+    /**
+    * 使用相对定位，并进行线性移动动画
+    * animation 属性：move 动画、持续 2s、线性移动、无限循环
+    * 其中，move 动画由 @keyframes 进行定制
+    */
+    position: relative;
+    animation: move 2s linear infinite;
+}
+
+/**
+ * @keyframes 定制 move 动画
+ * 内部定义了动画 0%、50% 和 100% 的位置
+ */
+@keyframes move {
+    0% {
+        left: 0
+    }
+    50% {
+        left: 200px
+    }
+    100% {
+        left: 0
+    }
+}
+</style>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -8,18 +8,22 @@
         <WidenAnimation></WidenAnimation>
     </div>
 
+    <div v-if="version === 3">
+        <MoveAnimation></MoveAnimation>
+    </div>
 </template>
 
 <script setup>
 
 import TodoList from "../components/TodoList"
 import WidenAnimation from "../components/WidenAnimation"
+import MoveAnimation from "@/components/MoveAnimation";
 
 /**
  * 控制显示页面的版本号
  * @type {number}
  */
-const version = 2
+const version = 3
 
 </script>
 
