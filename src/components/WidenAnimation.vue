e225a660a697402476a6c29cf2fe0bb1644e60dd
Linnzh <reg.lynnzh@gmail.com>
2022-11-19 17:30:54 +0800

    comment: 使用动画 - Animation

---
+++ src/components/WidenAnimation.vue
@@ -0,0 +1,23 @@
+<template>
+    <!-- 需求：每次点击按钮的时候，div 标签的宽度都增加 100px   -->
+    <div class="box" :style="{width: width+'px'}"></div>
+    <button @click="widenBox">Click to widen 100px!</button>
+</template>
+
+<script setup>
+import {ref} from "vue";
+
+let width = ref(100)
+
+function widenBox() {
+    width.value += 100
+}
+
+</script>
+
+<style scoped>
+.box {
+    background: #42b983;
+    height: 100px;
+}
+</style>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,12 +1,25 @@
 <template>
 
-  <TodoList/>
+    <div v-if="version === 1">
+        <TodoList></TodoList>
+    </div>
+
+    <div v-if="version === 2">
+        <WidenAnimation></WidenAnimation>
+    </div>
 
 </template>
 
 <script setup>
 
 import TodoList from "../components/TodoList"
+import WidenAnimation from "../components/WidenAnimation"
+
+/**
+ * 控制显示页面的版本号
+ * @type {number}
+ */
+const version = 2
 
 </script>
 
