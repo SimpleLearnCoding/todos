c56075ccdb3d6abec1197b48b07d07189686a9f1
Linnzh <reg.lynnzh@gmail.com>
2022-11-20 16:05:32 +0800

    comment: add question

--- src/store/index.js
+++ src/store/index.js
@@ -6,6 +6,10 @@ import { createStore } from 'vuex'
  * 注册方式：
  * 在项目入口文件（ex：src/main.js），使用 app.use(store) 进行注册
  *
+ * question: 什么时候的数据用 Vuex 管理，什么时候数据要放在组件内部使用 ref 管理呢？
+ * 对于一个数据，如果只是组件内部使用就是用 ref 管理；
+ * 如果我们需要跨组件，跨页面共享的时候，我们就需要把数据从 Vue 的组件内部抽离出来，放在 Vuex 中去管理。
+ *
  * @type {Store<{count: number}>}
  */
 const store = createStore({
