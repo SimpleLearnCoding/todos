8a40ff0ce7d245020fd52dab5d02d61af833a701
Linnzh <reg.lynnzh@gmail.com>
2022-11-20 14:44:45 +0800

    comment: 使用按钮控制标题文字的显示和隐藏 - transition 组件版

--- src/components/TransitionText.vue
+++ src/components/TransitionText.vue
@@ -1,6 +1,14 @@
 <template>
     <!--  需求：使用一个按钮控制标题文字的显示和隐藏  -->
-    <h1 v-if="showTitle">Hello, Transition Text</h1>
+
+    <!--  使用 transition 组件  -->
+    <!--  包裹需要动画的元素，并通过设置 transition 组件的 class 来控制动画  -->
+    <!--  例如这里设置了 fade 动画效果（褪色、渐隐）  -->
+    <!--  对应进（Enter）出（Leave）动画的 class 名应为：fade-enter-active、fade-leave-active  -->
+    <!--  另还需设置动画的进出开始（From）和结束（To）效果，class 名为：fade-enter-from、fade-leave-to  -->
+    <transition name="fade">
+        <h1 v-if="showTitle">Hello, Transition Text</h1>
+    </transition>
     <br>
     <button @click="toggle">Toggle Text</button>
 </template>
@@ -17,5 +25,11 @@
 </script>
 
 <style scoped>
+.fade-enter-active, .fade-leave-active {
+    transition: opacity 0.5s linear;
+}
 
+.fade-enter-from, .fade-leave-to {
+    opacity: 0;
+}
 </style>
