bdfb8bf2f51ba6d7c92d9b39b2e42f9fa2691ef0
Linnzh <reg.lynnzh@gmail.com>
2022-11-20 14:24:36 +0800

    comment: 使用按钮控制标题文字的显示和隐藏 - 普通版

---
+++ src/components/TransitionText.vue
@@ -0,0 +1,21 @@
+<template>
+    <!--  需求：使用一个按钮控制标题文字的显示和隐藏  -->
+    <h1 v-if="showTitle">Hello, Transition Text</h1>
+    <br>
+    <button @click="toggle">Toggle Text</button>
+</template>
+
+<script setup>
+    import {ref} from "vue";
+
+    let showTitle = ref(true)
+
+    function toggle() {
+        showTitle.value = !showTitle.value
+    }
+
+</script>
+
+<style scoped>
+
+</style>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -11,6 +11,10 @@
     <div v-if="version === 3">
         <MoveAnimation></MoveAnimation>
     </div>
+
+    <div v-if="version === 4">
+        <TransitionText></TransitionText>
+    </div>
 </template>
 
 <script setup>
@@ -18,12 +22,13 @@
 import TodoList from "../components/TodoList"
 import WidenAnimation from "../components/WidenAnimation"
 import MoveAnimation from "@/components/MoveAnimation";
+import TransitionText from "@/components/TransitionText";
 
 /**
  * 控制显示页面的版本号
  * @type {number}
  */
-const version = 3
+const version = 4
 
 </script>
 
