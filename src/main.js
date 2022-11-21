c34e0acdcee5a7d12a33e1b5b7d7618ed4cf760e
Linnzh <reg.lynnzh@gmail.com>
2022-11-21 09:59:13 +0800

    deps: 添加了 babel-plugin-vue 插件依赖 && 注册了 ant-design 组件 && 配置了 babelrc

--- src/main.js
+++ src/main.js
@@ -4,8 +4,13 @@ import App from './App.vue'
 import router from "./router/index";
 import store from "./store/index";
 
+import Antd from "ant-design-vue";
+
 /**
  * 使用 use() 加载 router 配置
  * 使用 use() 加载数据源 store
+ *
+ * 使用 use() 注册 Antd 组件 - 该方式为全局注册，更多请参考：
+ * @link https://antdv.com/docs/vue/getting-started-cn
  */
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(store).use(router).use(Antd).mount('#app')
